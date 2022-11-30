86bd55211d4009497348f5d3d0f2216823d465e1
KongTi <hjm0137@naver.com>
2022-11-30 14:39:44 +0900

    [Fix]rename.py value parameter

--- rename.py
+++ rename.py
@@ -1,10 +1,15 @@
 import os, glob
 
-os.chdir("train/100/")
+nation = "KR"
+price = "100"
+name = "Won"
+BorF = "B"
+
+os.chdir(f'{nation}/{price}/{BorF}')
 
 filelist=glob.glob("*")
 
 for i, file in enumerate(filelist):
     _, ext = os.path.splitext(file)
     number = str(i).zfill(3)
-    os.rename(file, f'{number}_KR_100 Won{ext}')
+    os.rename(file, f'{number}_{nation}_{price} {name}({BorF}){ext}')
